980cbe7ff7542da991c812c4a9aa577d33277097
connor <connordanielfrank@gmail.com>
2022-11-20 15:33:57 +1300

    build: Add dev / prod mode up script

---
+++ app/up.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Flags for dev
+while getopts "m:" arg
+do
+    case $arg in
+        m) MODE=${OPTARG};;
+        *) usage exit 1;;
+    esac
+done
+
+if [ -z "$MODE" ]
+then
+    echo "+------------------+"
+    echo "| Production Mode  |"
+    echo "+------------------+"
+    docker compose up
+elif [ "$MODE" == "dev" ]
+then
+    echo "+------------------+"
+    echo "| Development Mode |"
+    echo "+------------------+"
+    docker compose -f compose.yml -f compose.dev.yml up
+else
+    echo "Incorrect flag argument passed"
+    echo "options: 'dev', ''"
+    exit 1;
+fi
